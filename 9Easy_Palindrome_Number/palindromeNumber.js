840a684eb904caeccca855649a179d88444f8137
Vatsal Pande <vpande@ebay.com>
2019-03-13 15:44:22 -0700

    Easy Solution for reversing a number. Similar logic will be used for palindrome number

---
+++ 9Easy_Palindrome_Number/palindromeNumber.js
@@ -0,0 +1,8 @@
+(function IIFE() {
+   function isPalindromeNumber(number) {
+       if (Number.isNaN(number)) return false;
+       let isPalindrome = false;
+   }
+   const isPalindrome = isPalindromeNumber();
+   console.info(isPalindrome);
+}())
---
+++ Easy_ReverseAnumber/reverseANumber.js
@@ -0,0 +1,12 @@
+(function IIFE() {
+    function reverseNumber(number) {
+        let reversedNumber = 0;
+        while(number != 0) {    // 189
+            reversedNumber = reversedNumber*10; //0  //90 //988.9
+            reversedNumber = reversedNumber + number%10; //9 //98 //981
+            number = Math.floor(number/10); //18 1 0
+        }
+    }
+    const numberReversed = reverseNumber();
+    console.info(numberReversed);
+}())
