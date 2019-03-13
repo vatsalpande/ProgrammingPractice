07d50ce1a80fb97b473b266a473e4876c1e1d7fa
Vatsal Pande <vpande@ebay.com>
2019-03-13 16:10:47 -0700

    Reverse Number not working with negative numbers

--- 9Easy_Palindrome_Number/palindromeNumber.js
+++ 9Easy_Palindrome_Number/palindromeNumber.js
@@ -1,7 +1,17 @@
 (function IIFE() {
+    function reversedNumber(number) {
+        let reversed = 0;
+        while(number !==0) {
+            reversed = reversed*10;                  0
+            reversed = reversed + number%10;        // -1
+            number = Math.floor(number/10);
+        }
+        return reversed;
+    }
    function isPalindromeNumber(number) {
        if (Number.isNaN(number)) return false;
-       let isPalindrome = false;
+       const reverse = reversedNumber(number);
+       return number == reverse
    }
    const isPalindrome = isPalindromeNumber();
    console.info(isPalindrome);
