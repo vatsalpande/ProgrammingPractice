b27b3af39ad3649e3f89fff8b6fa7cf8329b3d28
Vatsal Pande <vpande@ebay.com>
2019-03-15 22:30:30 -0700

    All Triplets in an array. All Combinations. Looks good n^3 time. Is it correct? Any better solution?

--- 9000 All_Triplets_Array/triplets.js
+++ 9000 All_Triplets_Array/triplets.js
@@ -3,27 +3,15 @@
     function allPossibleTriplets(array) {
         const possibleTriplets = [];
         for (let i=0; i<array.length; i++) {
-            let innerTriples = [array[i]];
-                for (let j=0; j< array.length; j++) {
-                    if (i!==j) {
-                        innerTriples.push(array[j])
-                        for (let z=0; z<array.length; z++) {
-                            if (z !==i && z !== j) {
-                                possibleTriplets.push([array[i], array[j], array[z]]);
-
-
-                                // if (innerTriples.length == 3) {
-                                //     possibleTriplets.push(innerTriples);
-                                //     innerTriples = [array[i], array[j]];
-                                // } else {
-                                //     innerTriples.push(array[z])
-                                // }
-                            }
-
+            for (let j=0; j< array.length; j++) {
+                if (i!==j) {
+                    for (let z=0; z<array.length; z++) {
+                        if (z !==i && z !== j) {
+                            possibleTriplets.push([array[i], array[j], array[z]]);
                         }
                     }
+                }
             }
-
         }
         return possibleTriplets;
     }
